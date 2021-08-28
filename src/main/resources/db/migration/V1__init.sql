3995e3c59a68f696928a1d21c9def38c9a80ca23
BuzzyMO <oleksandr.lynnyk@nure.ua>
2021-08-28 19:08:40 +0300

    add pizzeria address and user authorities

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -9,6 +9,24 @@ CREATE TABLE users
   created_at   TIMESTAMPTZ DEFAULT now()
 );
 
+CREATE TABLE authorities
+(
+  id    INT PRIMARY KEY,
+  value TEXT NOT NULL
+);
+
+CREATE TABLE user_authorities
+(
+  user_id      BIGINT REFERENCES users (id),
+  authority_id INT REFERENCES authorities (id),
+  PRIMARY KEY (user_id, authority_id)
+);
+
+insert into authorities (id, value)
+values (0, 'ROLE_USER');
+insert into authorities (id, value)
+values (1, 'ROLE_ADMIN');
+
 CREATE TABLE ingredients
 (
   id   SERIAL PRIMARY KEY,
@@ -31,16 +49,28 @@ CREATE TABLE pizza_ingredients
   PRIMARY KEY (pizza_id, ingredient_id)
 );
 
+CREATE TABLE pizzeria
+(
+  id        SERIAL PRIMARY KEY,
+  city      TEXT NOT NULL,
+  street    TEXT NOT NULL,
+  building  TEXT NOT NULL,
+  latitude  REAL NOT NULL,
+  longitude REAL NOT NULL
+);
+
 CREATE TABLE deliveries
 (
-  id        BIGSERIAL PRIMARY KEY,
-  user_id   BIGSERIAL REFERENCES users (id),
-  city      TEXT  NOT NULL,
-  street    TEXT  NOT NULL,
-  building  TEXT  NOT NULL,
-  apartment TEXT,
-  exp_time  TIME  NOT NULL,
-  cost      MONEY NOT NULL
+  id            BIGSERIAL PRIMARY KEY,
+  created_by    BIGSERIAL REFERENCES users (id),
+  pizzeria_from INT   NOT NULL REFERENCES pizzeria (id),
+  city          TEXT  NOT NULL,
+  street        TEXT  NOT NULL,
+  building      TEXT  NOT NULL,
+  apartment     TEXT,
+  distance_m    INT   NOT NULL,
+  exp_time      TIME  NOT NULL,
+  cost          MONEY NOT NULL
 );
 
 CREATE TABLE orders
